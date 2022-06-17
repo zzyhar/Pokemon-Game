066fc827b419feecb489d0c5b8aa5978193bf999
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-06-17 13:57:16 -0400

    Adding Foreground Objects

--- index.js
+++ index.js
@@ -10,22 +10,6 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, i + 70));
 }
 
-class Boundary {
-  // res
-  static width = 48;
-  static height = 48;
-
-  constructor({ position }) {
-    this.position = position;
-    this.width = 48;
-    this.height = 48;
-  }
-  draw() {
-    c.fillStyle = "rgba(255,0,0,0)";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
-
 const boundaries = [];
 
 const offset = {
@@ -50,34 +34,11 @@ collisionsMap.forEach((row, i) => {
 const image = new Image();
 image.src = "./img/Pellet Town.png";
 
+const foregroundImage = new Image();
+foregroundImage.src = "./img/foregroundObjects.png";
+
 const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
-
-// Make the illusion that background is moving with the hero
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position;
-    this.image = image;
-    this.frames = frames;
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max;
-      this.height = this.image.height;
-    };
-  }
-  draw() {
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    );
-  }
-}
 
 const player = new Sprite({
   position: {
@@ -98,6 +59,14 @@ const background = new Sprite({
   image: image,
 });
 
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
+});
+
 const keys = {
   w: {
     pressed: false,
@@ -113,7 +82,7 @@ const keys = {
   },
 };
 
-const movables = [background, ...boundaries];
+const movables = [background, ...boundaries, foreground];
 
 function rectangleCollision({ rectangle1, rectangle2 }) {
   return (
@@ -131,6 +100,7 @@ function animate() {
     boundary.draw();
   });
   player.draw();
+  foreground.draw();
 
   // moving hero  through map by keys
   let moving = true;
