cd3b9bb5e37fa744b851e48cd59173e11700d883
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-06-16 20:04:14 -0400

    Player to Map Boundary Collisions

--- index.js
+++ index.js
@@ -1,114 +1,275 @@
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d'); // c = content
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d"); // c = content
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
-c.fillStyle = 'white'
-c.fillRect(0, 0, canvas.width, canvas.height)
+// Editing Boundaries for map objects Section
+const collisionsMap = [];
+for (let i = 0; i < collisions.length; i += 70) {
+  collisionsMap.push(collisions.slice(i, i + 70));
+}
 
-const image = new Image()
-image.src = './img/Pellet Town.png'
+class Boundary {
+  // res
+  static width = 48;
+  static height = 48;
 
-const playerImage = new Image()
-playerImage.src = './img/playerDown.png'
+  constructor({ position }) {
+    this.position = position;
+    this.width = 48;
+    this.height = 48;
+  }
+  draw() {
+    c.fillStyle = "rgba(255,0,0,0)";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+const boundaries = [];
+
+const offset = {
+  x: -740,
+  y: -620,
+};
+
+collisionsMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+const image = new Image();
+image.src = "./img/Pellet Town.png";
+
+const playerImage = new Image();
+playerImage.src = "./img/playerDown.png";
 
 // Make the illusion that background is moving with the hero
-class Sprite { 
-    constructor({ position, velocity, image }){
-    this.position = position
-    this.image = image
-    }
-    draw(){
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
+class Sprite {
+  constructor({ position, velocity, image, frames = { max: 1 } }) {
+    this.position = position;
+    this.image = image;
+    this.frames = frames;
+    this.image.onload = () => {
+      this.width = this.image.width / this.frames.max;
+      this.height = this.image.height;
+    };
+  }
+  draw() {
+    c.drawImage(
+      this.image,
+      0,
+      0,
+      this.image.width / this.frames.max,
+      this.image.height,
+      this.position.x,
+      this.position.y,
+      this.image.width / this.frames.max,
+      this.image.height
+    );
+  }
 }
+
+const player = new Sprite({
+  position: {
+    x: canvas.width / 2 - 192 / 4 / 2,
+    y: canvas.height / 2 - 68 / 2,
+  },
+  image: playerImage,
+  frames: {
+    max: 4,
+  },
+});
 
 const background = new Sprite({
-    position:{
-        x: -740, 
-        y: -595
-    },
-    image: image
-})
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: image,
+});
 
 const keys = {
-    w: {
-        pressed: false
-    },
-    a: {
-        pressed: false
-    },
-    s: {
-        pressed: false
-    },
-    d: {
-        pressed: false
-    }
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  s: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
+
+const movables = [background, ...boundaries];
+
+function rectangleCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.position.x + rectangle1.width >= rectangle2.position.x &&
+    rectangle1.position.x <= rectangle2.position.x + rectangle2.width &&
+    rectangle1.position.y + rectangle1.height >= rectangle2.position.y &&
+    rectangle1.position.y <= rectangle2.position.y + rectangle2.height
+  );
 }
 
+function animate() {
+  window.requestAnimationFrame(animate);
+  background.draw();
+  boundaries.forEach((boundary) => {
+    boundary.draw();
+  });
+  player.draw();
 
-function animate(){
-    window.requestAnimationFrame(animate)
-    background.draw()
-    c.drawImage(playerImage,
-    // cropping (x, y) and width and height of cropped img
-    0, 
-    0,
-    playerImage.width /4,
-    playerImage.height,
-    // actual cordinatres and size 
-    canvas.width / 2 - (playerImage.width / 4) /2,
-    canvas.height / 2 - playerImage.height /2,
-    playerImage.width / 4,
-    playerImage.height
-    )
-    // moving hero  through map by keys
-    if(keys.w.pressed && lastKey === 'w') background.position.y += 3
-    else if(keys.a.pressed && lastKey === 'a') background.position.x += 3
-    else if(keys.s.pressed && lastKey === 's') background.position.y -= 3
-    else if(keys.d.pressed && lastKey === 'd') background.position.x -= 3
-    
+  // moving hero  through map by keys
+  let moving = true;
+  if (keys.w.pressed && lastKey === "w") {
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangleCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y + 3,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.y += 3;
+      });
+  } else if (keys.a.pressed && lastKey === "a") {
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangleCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x + 3,
+              y: boundary.position.y,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.x += 3;
+      });
+  } else if (keys.s.pressed && lastKey === "s") {
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangleCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x,
+              y: boundary.position.y - 3,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.y -= 3;
+      });
+  } else if (keys.d.pressed && lastKey === "d") {
+    for (let i = 0; i < boundaries.length; i++) {
+      const boundary = boundaries[i];
+      if (
+        rectangleCollision({
+          rectangle1: player,
+          rectangle2: {
+            ...boundary,
+            position: {
+              x: boundary.position.x - 3,
+              y: boundary.position.y,
+            },
+          },
+        })
+      ) {
+        console.log("colliding");
+        moving = false;
+        break;
+      }
+    }
+    if (moving)
+      movables.forEach((movable) => {
+        movable.position.x -= 3;
+      });
+  }
 }
-animate()
+animate();
 
+let lastKey = "";
 
-let lastKey = '' 
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = true;
+      lastKey = "w";
+      break;
+    case "a":
+      keys.a.pressed = true;
+      lastKey = "a";
+      break;
+    case "s":
+      keys.s.pressed = true;
+      lastKey = "s";
+      break;
+    case "d":
+      keys.d.pressed = true;
+      lastKey = "d";
+      break;
+  }
+});
 
-window.addEventListener('keydown', (e) => {
-    switch (e.key){
-        case 'w': 
-        keys.w.pressed = true 
-        lastKey = 'w'
-        break
-        case 'a': 
-        keys.a.pressed = true 
-        lastKey = 'a'
-        break
-        case 's': 
-        keys.s.pressed = true 
-        lastKey = 's'
-        break
-        case 'd': 
-        keys.d.pressed = true 
-        lastKey = 'd'
-        break
-    }
-})
-
-window.addEventListener('keyup', (e) => {
-    switch (e.key){
-        case 'w': 
-        keys.w.pressed = false 
-        break
-        case 'a': 
-        keys.a.pressed = false 
-        break
-        case 's': 
-        keys.s.pressed = false
-        break
-        case 'd': 
-        keys.d.pressed = false
-        break
-    }
-})
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "w":
+      keys.w.pressed = false;
+      break;
+    case "a":
+      keys.a.pressed = false;
+      break;
+    case "s":
+      keys.s.pressed = false;
+      break;
+    case "d":
+      keys.d.pressed = false;
+      break;
+  }
+});
