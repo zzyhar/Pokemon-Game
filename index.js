9a928e009d05fcaf0453a5f7d43c8519b6534c3c
zzyhar <93952085+zzyhar@users.noreply.github.com>
2022-06-20 18:16:16 -0400

    Attack - Fireball

--- index.js
+++ index.js
@@ -355,24 +355,26 @@ const emby = new Sprite({
   animate: true,
 });
 
+const renderedSprites = [draggle, emby];
+
 function animateBattle() {
   window.requestAnimationFrame(animateBattle);
   battleBackground.draw();
-  draggle.draw();
-  emby.draw();
+
+  renderedSprites.forEach((sprite) => {
+    sprite.draw();
+  });
 }
 
 animateBattle();
-
+// event listeners for  attack buttons
 document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (e) => {
+    const selectedAttack = attacks[e.currentTarget.innerHTML];
     emby.attack({
-      attack: {
-        name: "Tackle",
-        damage: 10,
-        type: "Normal",
-      },
+      attack: selectedAttack,
       recipient: draggle,
+      renderedSprites,
     });
   });
 });
